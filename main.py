ba169eb82da501b0885547b3041f3c97aaf6cb1f
Ericaf06 <108380214+Ericaf06@users.noreply.github.com>
2023-08-02 00:34:07 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-
-
-'''Imports'''
+"""Imports"""
 import arcade
 import math
-from math import sin,cos, degrees, atan2
+from math import atan2, degrees
 import pathlib
 
-'''Global variables'''
+"""Global variables"""
 PARENT_DIR = pathlib.Path(__file__).parent
 SCREEN_WIDTH = 1500
 SCREEN_HEIGHT = 1000
@@ -23,52 +21,47 @@ RIGHT_FACING = 0
 BULLET_SPEED = 20
 BOSS_SPEED = 10
 
-def load_texture_pair(filename):
-    '''Loads players textures'''
-    return [
-        arcade.load_texture(filename),
-        arcade.load_texture(filename, flipped_horizontally=True)]
     
 class MyGame(arcade.Window):
-    '''Game class '''
+    """Game class"""
+
     def __init__(self):
         super().__init__()
         self.setup()
         self.tile_map = None
         self.score = 0
-        self.enemy_list = None
-        self.enemy_2_list = None
         self.health_list = None
         self.level = 1
-    
+        #Loads Mp3 sounds used in game
         self.game_over_sound = arcade.load_sound(
-            ':resources:sounds/hurt3.wav'
+            'lose_sound.mp3'
         )
         self.game_win_sound = arcade.load_sound(
-            ':resources:sounds/upgrade2.wav'
+            'win_sound.mp3'
         )
         self.climbing_sound = arcade.load_sound(
-            ':resources:sounds/upgrade4.wav'
+            'climbing_sound.mp3'
         )
         self.coin_sound = arcade.load_sound(
-            ':resources:sounds/coin1.wav'
+            'coin_sound.mp3'
         )
         self.jump_sound = arcade.load_sound(
-            ':resources:sounds/jump1.wav'
+            'jump_sound.mp3'
         )
         self.kill_sound = arcade.load_sound(
-            ':resources:sounds/hurt3.wav'
+            'ouch_sound.mp3'
         )
         self.bullet_sound = arcade.load_sound(
-            ':resources:sounds/laser3.wav'
+            'bullet_sound.mp3'
         )
         self.bullet = arcade.Sprite(
-            ':resources:images/space_shooter/laserBlue01.png'
+            PARENT_DIR/ 'Bullet-0006.png'
         )
       
         
     def setup(self):
-        '''Set up function for game, loads scene, map and, sprites'''
+        """Set up function for game, loads scene, map and, sprites"""
+        
         arcade.set_background_color(arcade.color.CEIL)
         layer_options = {
             'Ground': {
@@ -76,38 +69,55 @@ class MyGame(arcade.Window):
             }
         }
         self.level = 1
-        self.tile_map = PARENT_DIR / f"./assets/LEVEL_{self.level}.tmx",
+        #Loads in tile map and adds scene from tile map
+        self.tile_map = PARENT_DIR / f"./assets/LEVEL_{self.level}.tmx"
         layer_options=layer_options
-        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+        self.scene = arcade.Scene.from_tilemap(self.level)
+        #Creates bullet list and adds to scene
+        #Adds player to scene by referencing player class
         self.bullet_list = arcade.SpriteList()
+        self.scene.add_sprite_list(
+            'bullets',sprite_list=self.bullet_list
+        )
         self.player = Player()
-        self.scene.add_sprite_list('bullets',sprite_list=self.bullet_list)
-        self.scene.add_sprite_list('player',sprite_list = self.player)
+        self.scene.add_sprite_list(
+            'player',sprite_list = self.player
+            )
+        #Creates physics engine, allows game to follow laws of physics
         self.physics_engine = arcade.PhysicsEnginePlatformer(
             self.player,
-            walls =self.scene['Ground'], 
+            walls=self.scene['Ground'], 
             ladders=self.scene['Ladders'],
             gravity_constant=GRAVITY,
         )
-
-        self.camera = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
-        self.HUD_camera = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
+        #Creates cameras 
+        self.camera = arcade.Camera(
+            SCREEN_WIDTH, SCREEN_HEIGHT
+            )
+        self.HUD_camera = arcade.Camera(
+            SCREEN_WIDTH, SCREEN_HEIGHT
+            )
+        #Creates health list and adds to scene
         self.health = 5
         self.health_list = arcade.SpriteList()
         for i in range(5):
-            health = arcade.Sprite(":resources:images/space_shooter/playerLife1_green.png")
+            health = arcade.Sprite(
+                PARENT_DIR/ 'Health-0008.png'
+                )
             health.center_x = 35 + 40 * i
             health.center_y = SCREEN_HEIGHT - 100
             self.health_list.append(health)
         
     def on_draw(self):
-        '''Draws lists and text'''
+        """Draws lists and text"""
+
         self.clear()
         self.camera.use()
         self.bullet_list.draw()
-        '''self.health_list.draw()'''
+        #self.health_list.draw()
         self.scene.draw()
         self.HUD_camera.use()
+        #Draws the score text
         score_text = f"Score: {self.score}"
         arcade.draw_text(
             score_text,
@@ -116,7 +126,11 @@ class MyGame(arcade.Window):
             arcade.csscolor.BLACK,
             20,
         )
+
     def player_camera(self, x, y):
+        """Takes coordinates of player and screen
+        Which allows camera to centre on the player"""
+
         screen_center_x = self.player.center_x - (
             self.camera.viewport_width / 2)
         screen_center_y = self.player.center_y - (
@@ -131,10 +145,15 @@ class MyGame(arcade.Window):
         self.camera.move_to(player_centered)
         
     def on_mouse_press(self, x, y, button, modifiers):
-        '''Called when mouse is pressed'''
+        """Called when mouse is pressed"""
+
         bullet = arcade.Sprite(
-            ':resources:images/space_shooter/laserBlue01.png'
+            PARENT_DIR/'Bullet-0006.png'
         )
+        #Ensures bullet spawns from players x,y coordinates
+        #Takes difference between x,y and start x,y
+        #Using atan2, the difference taken allows bullet
+        #To move in any direction the mouses' x and y are
         start_x = self.player.center_x
         start_y = self.player.center_y
         bullet.center_x = start_x
@@ -152,24 +171,34 @@ class MyGame(arcade.Window):
         self.bullet_list.append(bullet)
 
     def on_update(self, dt):
-        '''Updates scene'''
+        """Updates scene"""
         self.bullet_list.update()
         self.physics_engine.update()
         self.player.update_animation()
-        camera_x = self.player.center_x /2
-        camera_y = self.player.center_y /2
-       # self.center_camera_to_player()
+        camera_x = self.player.center_x / 2
+        camera_y = self.player.center_y / 2
 
         if camera_x < 0:
             camera_x = 0
 
         if camera_y < 20:
             camera_y = 20
-            
         self.camera.move_to((camera_x, camera_y))
 
+        #Checks for colisions with plsyer and tile layer
+        hurt = arcade.check_for_collision_with_list(
+            self.player, self.scene['Hurt'])
+        
         danger = arcade.check_for_collision_with_list(
             self.player, self.scene['Enemy'])
+        #If the player collides with the scene layer 
+        #Stated in the hurt function, health will decrease by 1
+        if hurt:
+            self.player.center_x =400
+            self.player.center_y = 400
+            arcade.play_sound(self.kill_sound)
+            self.health_list.pop()
+            game_over = GameOverView
 
         if danger:
             self.player.center_x = 400
@@ -181,17 +210,20 @@ class MyGame(arcade.Window):
               self.player.kill()
              arcade.play_sound(self.game_over_sound)
                 self.window.show_view(game_over)'''
-
-
+        
+        #Kills bullet after it has travelled past these points
+        #This helps the game from crashing if too many bullets are fired
         if self.bullet.center_x > 1000 or self.bullet.center_y < 0:
             self.bullet.kill()
 
+        #If player collides with coins score increases by 1
         coins = arcade.check_for_collision_with_list(
             self.player, self.scene['Coins'])
         for coin in coins:
             self.score += 1
             coin.kill()
 
+        #Is able to shoot enemy and kill them with bullets 
         for self.bullet in self.bullet_list:
             enemy_bullet = arcade.check_for_collision_with_list(
                 self.bullet, self.scene['Enemy'])
@@ -200,7 +232,7 @@ class MyGame(arcade.Window):
                 enemy_bullet[0].kill()
 
     def on_key_press(self, symbol, modifiers):
-        '''Called when key is pressed, controls player movement'''
+        """Called when key is pressed. Controls player movement"""
         if symbol == arcade.key.D:
             self.player.change_x = PLAYER_MOVEMENT_SPEED
         elif symbol == arcade.key.A:
@@ -216,13 +248,9 @@ class MyGame(arcade.Window):
         elif symbol == arcade.key.Q:
             if self.physics_engine.is_on_ladder():
                 self.player.change_y = -PLAYER_MOVEMENT_SPEED
-        if symbol == arcade.key.X:
-            self.health_list.pop()
-            if len(self.health_list) <= 0:
-                self.player.kill()
 
     def on_key_release(self, symbol, modifiers):
-        '''Called when key is released'''
+        """Called when key is released"""
         if symbol == arcade.key.D:
             self.player.change_x = 0
         if symbol == arcade.key.A:
@@ -239,70 +267,21 @@ class MyGame(arcade.Window):
 
         
 class Player(arcade.Sprite):
-    '''Player class which manages players position, textures and movement'''
+    """Player class which manages players position, textures and movement"""
+
     def __init__(self):
         super().__init__(
             ':resources:images/animated_characters/male_adventurer/maleAdventurer_idle.png'
         )
         self.center_x = 400
         self.center_y = 400
-        self.idle_textures = load_texture_pair(
-            ':resources:images/animated_characters/male_adventurer/maleAdventurer_idle.png'
-        )
-        self.walk_textures = []
-        for i in range(2):
-            frames = load_texture_pair(
-                f':resources:images/animated_characters/male_adventurer/maleAdventurer_idle.png'
-        )
-            self.walk_textures.append(frames)
-        self.fall_textures = load_texture_pair(
-            ':resources:images/animated_characters/male_adventurer/maleAdventurer_fall.png'
-        )
-        self.jump_textures = load_texture_pair(
-            ':resources:images/animated_characters/male_adventurer/maleAdventurer_jump.png'
-        )
-        self.climb_textures = load_texture_pair(
-            ':resources:images/animated_characters/male_adventurer/maleAdventurer_climb0.png'
-        )
         self.face_direction = RIGHT_FACING
         self.walk_index = 0
         self.odo = 0
-    
-
-    def update_animation(self):
-        '''Updates player movement and movement textures'''
-        if self.odo < 2:
-            self.odo += 1
-            return
-        self.odo = 0
-
-        if self.change_x > 0:
-            self.face_direction = RIGHT_FACING
-        if self.change_x < 0:
-            self.face_direction = LEFT_FACING
-
-        if self.change_y > 0:
-            self.texture = self.jump_textures[self.face_direction]
-            return
-        if self.change_y > 0:
-            self.texture = self.climb_textures[self.face_direction]
-            return
-        if self.change_y < 0:
-            self.texture = self.fall_textures[self.face_direction]
-            return
-
-        if self.change_x != 0:
-            self.walk_index += 1
-            self.walk_index = self.walk_index % 2
-            self.texture = self.walk_textures[
-                self.walk_index][self.face_direction]
-            return
-
-        self.texture = self.idle_textures[self.face_direction]
-
 
 class WelcomeView(arcade.View):
-    '''View that shows when game is loaded'''
+    """View that shows when game is loaded"""
+
     def __init__(self):
         super().__init__()
         arcade.set_background_color(arcade.color.AFRICAN_VIOLET)
@@ -321,9 +300,15 @@ class WelcomeView(arcade.View):
 
 
 class GameWinView(arcade.View):
-    '''View that shows when the player wins'''
+    """View that shows when the player wins"""
+
     def __init__(self):
         super().__init__()
+        arcade.set_background_color(arcade.color.BRILLIANT_LAVENDER)
+
+    
+    def on_show(self):
+        arcade.set_background_color(arcade.color.BRILLIANT_LAVENDER)
 
     def on_draw(self):
         self.clear()
@@ -336,9 +321,14 @@ class GameWinView(arcade.View):
 
 
 class GameOverView(arcade.View):
-    '''View that shows when player dies/loses'''
+    """View that shows when player dies/loses"""
+
     def __init__(self):
         super().__init__()
+        arcade.set_background_color(arcade.color.BULGARIAN_ROSE)
+
+    def on_show(self):
+        arcade.set_background_color(arcade.color.BULGARIAN_ROSE)
 
     def on_draw(self):
         self.clear()
@@ -351,7 +341,8 @@ class GameOverView(arcade.View):
 
 
 class Game(arcade.Window):
-    '''Manages different game views and windows'''
+    """Manages different game views and windows"""
+
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE)
         self.game_view = MyGame()
@@ -360,6 +351,7 @@ class Game(arcade.Window):
         self.game_over = GameOverView()
         self.window.show_view(welcome_view)
 
+#Allows game to run 
 def main():
     game = MyGame()
     arcade.run()
